f0750eacbaef8b437a91eb1f1a17b50de528bcf2
Jason Mei <jason.mei403@gmail.com>
2018-07-21 20:20:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,8 @@ class Song
 
   def initialize(name, artist = nil, genre = nil)
     @name = name
-    self.artist = artist if artist
-    self.genre = genre if genre
+    self.artist = artist #if artist
+    self.genre = genre #if genre
   end
 
   def self.all
